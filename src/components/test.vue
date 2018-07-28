766e5d317423b1fb95eada633ed32036e2113c31
zhengyx <1462977043@qq.com>
2018-07-28 17:05:19 +0800

    00

---
+++ src/components/test.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div>11111111</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+div {
+   color: red;
+
+}
+
+</style>
